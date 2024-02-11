063e5b0161136e137e6e63b027dc3436e5f5eaa0
Creverrr <158093869+Creverrr@users.noreply.github.com>
2024-02-11 23:16:55 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 from random import randint
-random_values_array=[] # по факту для цієї задачі непотрібний масив, тому що можна просто вивести рандомні числа та не тратити пам'ять + час виконання
+random_values_array=[] 
 for i in range(9):
     random_values_array.append(randint(17, 31))
 print(*random_values_array)
